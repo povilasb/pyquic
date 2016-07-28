72bd63b2179598bac6a327e1057f18455e2f67ee
Povilas Balciunas <balciunas90@gmail.com>
2016-07-28 23:18:30 +0300

    Add tag container class

---
+++ quic/tags.py
@@ -0,0 +1,45 @@
+from collections import OrderedDict
+
+from typing import Iterator, Union, List, Tuple
+
+
+class Container(dict):
+    """QUIC message tags container.
+
+    It keeps tags ordered in increasing order by tag name.
+    Which is a 32 bit number.
+    """
+    def __init__(self, init_tags: dict=None) -> None:
+        if init_tags:
+            for key, value in init_tags.items():
+                self.__setitem__(key, value)
+
+    def items(self) -> List[Tuple[int, bytes]]:
+        """
+        Returns:
+            tags key value pairs sorted by tag in ascending order.
+        """
+        return sorted(super().items())
+
+    def keys(self) -> List[int]:
+        """
+        Returns:
+            tag sorted in ascending order.
+        """
+        return sorted(super().keys())
+
+    def __setitem__(self, key: Union[str, int], value: str) -> None:
+        super().__setitem__(_int_tag_name(key), value)
+
+    def __getitem__(self, key: Union[str, int]) -> bytes:
+        return super().__getitem__(_int_tag_name(key))
+
+
+def _int_tag_name(tag: Union[str, int]) -> int:
+    """Ensures that given tag name is integer.
+
+    If it's not integer, it's converted to one.
+    """
+    if type(tag) == str:
+        tag = int.from_bytes(bytes(tag, 'ascii'), byteorder='little')
+    return tag
---
+++ tests/unit/test_tags_container.py
@@ -0,0 +1,54 @@
+from hamcrest import assert_that, is_
+
+import quic.tags as tags
+
+
+def describe_tags_container():
+    def describe__setitem__():
+        def it_sets_tag_value_than_can_be_retrieved_later():
+            msg_tags = tags.Container()
+            msg_tags['TAG1'] = 'value1'
+
+            assert_that(msg_tags['TAG1'], is_('value1'))
+
+        def it_accepts_32_bit_ints_as_tag_values():
+            msg_tags = tags.Container()
+            msg_tags[0x01020304] = 'value1'
+
+            assert_that(msg_tags[0x01020304], is_('value1'))
+
+        def it_converts_string_keys_to_ints_in_little_endian_before_storing():
+            msg_tags = tags.Container()
+            msg_tags['1234'] = 'value1'
+
+            assert_that(msg_tags[0x34333231], is_('value1'))
+
+    def describe_items():
+        def it_returns_items_in_the_ascending_order_by_tag_name_int_value():
+            msg_tags = tags.Container()
+            msg_tags['V'] = 'value1'
+            msg_tags['P'] = 'value2'
+            msg_tags['A'] = 'value3'
+
+            stored_tags = [t for t, _ in msg_tags.items()]
+
+            assert_that(stored_tags, is_([0x41, 0x50, 0x56]))
+
+    def describe_keys():
+        def it_returns_keys_in_the_ascending_order_by_tag_name_int_value():
+            msg_tags = tags.Container()
+            msg_tags['V'] = 'value1'
+            msg_tags['P'] = 'value2'
+            msg_tags['A'] = 'value3'
+
+            stored_tags = [t for t in msg_tags.keys()]
+
+            assert_that(stored_tags, is_([0x41, 0x50, 0x56]))
+
+    def describe_constructor():
+        def describe_when_non_empty_dictionary_is_provided():
+            def it_converts_dictionary_keys_into_ints_and_stores_the_values():
+                msg_tags = tags.Container({'1': 'value1', '2': 'value2'})
+
+                assert_that(msg_tags.items(),
+                    is_([(0x31, 'value1'), (0x32, 'value2')]))
